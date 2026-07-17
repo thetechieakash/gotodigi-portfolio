46c6bf73d1c3f82d2b288f0e953c274b95da32d4
akash halder <akashhalder277@gmail.com>
2026-07-18 01:40:05 +0530

    fix syntax

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import Home from "./Home.jsx"
-import About from "./About.jsx"
-import Blogs from "./Blogs.jsx"
-import BlogDetail from "./BlogDetail.jsx"
-import Portfolio from "./Portfolio.jsx"
-import Testimonial from "./Testimonial.jsx"
-import Faq from "./Faq.jsx"
-import Contact from "./Contact.jsx"
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Blogs from "./Blogs.jsx";
+import BlogDetail from "./BlogDetail.jsx";
+import Portfolio from "./Portfolio.jsx";
+import Testimonial from "./Testimonial.jsx";
+import Faq from "./Faq.jsx";
+import Contact from "./Contact.jsx";
 
 export {
     Home,
@@ -15,5 +15,5 @@ export {
     Portfolio,
     Testimonial,
     Faq,
-    Contact
+    Contact,
 }
